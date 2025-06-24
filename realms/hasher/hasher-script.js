cb02d0a158d68cf3ad2a4b99a9c0c267febc687e
Lavander Vokda <evelynjolly54@gmail.com>
2025-06-24 18:57:23 -0400

    Update hasher-script.js

--- realms/hasher/hasher-script.js
+++ realms/hasher/hasher-script.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const candyImg = document.getElementById("hasher-candy");
   if (candyImg) {
     candyImg.addEventListener("click", () => {
+      // Redirect to main hasher subpage or GitHub page
       window.location.href = "https://goldenmoonwitch.github.io/peachbonearchives/realms/hasher/";
     });
   }
 });
-
